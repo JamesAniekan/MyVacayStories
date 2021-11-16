1a80c8be1f21850291ef766287041fcb77649648
AniekanJames <jamesanny20@gmail.com>
2021-11-16 17:03:02 +0100

    Log in screen(Rough work) and navigation component.

---
+++ app/src/main/java/com/android/example/myvacaystories/ViewModels/SignInRegisterViewModel.kt
@@ -0,0 +1,8 @@
+package com.android.example.myvacaystories.ViewModels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SignInRegisterViewModel(application: Application): AndroidViewModel(application) {
+
+}
