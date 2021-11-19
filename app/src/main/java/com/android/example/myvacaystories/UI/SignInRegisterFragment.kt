4504985c4f17cdc7647003ac9f5c16522c48cafc
AniekanJames <jamesanny20@gmail.com>
2021-11-19 12:50:49 +0100

    Data binding and constraint layout.

--- app/src/main/java/com/android/example/myvacaystories/UI/SignInRegisterFragment.kt
+++ app/src/main/java/com/android/example/myvacaystories/UI/SignInRegisterFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.android.example.myvacaystories.R
+import com.android.example.myvacaystories.databinding.FragmentSignInRegisterBinding
 
 class SignInRegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in_register, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_in_register, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.registerButton.setOnClickListener {
+            binding.etName.visibility = View.VISIBLE
+        }
+        binding.signInButton.setOnClickListener {
+            binding.etName.visibility = View.GONE
+        }
     }
 }
